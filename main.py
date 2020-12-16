7f630b7a829a9268a6061d4e734051f991193b0c
Maxime Rauer <maxime.rauer@gmail.com>
2020-12-15 19:00:08 -0600

    [MID] Get dividends data

--- main.py
+++ main.py
@@ -2,17 +2,15 @@ import sys
 
 from lib import grab
 
-# div = g.get_dividends_history('PSX')
-
 
 def main():
     args = sys.argv
     g = grab.Grab()
     if args[1] == 'tickers':
-        print(g.get_tickers())
+        return g.get_tickers()
 
     if args[1] == 'dividends':
-        print(g.get_dividends_history('KO'))
+        return g.get_dividends()
 
 
 if __name__ == "__main__":
