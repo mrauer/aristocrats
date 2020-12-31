cc14d370ecb34ab794b2f19e93acac2da16dfe5a
Maxime Rauer <maxime.rauer@gmail.com>
2020-12-30 20:59:05 -0600

    [MID] Convert Chart Data (#11)

--- Makefile
+++ Makefile
@@ -5,7 +5,10 @@ run:
 	docker build -t aristocrats:latest . && docker run --env-file .env -it --rm -v ${CURDIR}:/usr/src/app aristocrats:latest
 
 grab:
-	python3 main.py $(source)
+	python3 main.py grab $(source)
+
+convert:
+	python3 main.py convert
 
 save:
 	sh lib/save.sh
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import sys
 
-from lib import grab
+from lib import convert, grab
+
+GRAB_KW = 'grab'
+CONVERT_KW = 'convert'
 
 
 def main():
     args = sys.argv
     g = grab.Grab()
-    if args[1] == 'tickers':
+    if args[1] == GRAB_KW and args[2] == 'tickers':
         return g.get_tickers()
 
-    if args[1] == 'dividends':
+    if args[1] == GRAB_KW and args[2] == 'dividends':
         return g.get_dividends()
+
+    if args[1] == CONVERT_KW:
+        c = convert.Convert()
+        return c.convert_data()
 
 
 if __name__ == "__main__":
